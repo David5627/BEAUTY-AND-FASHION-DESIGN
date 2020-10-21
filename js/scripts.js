61ddb7d766aa6368c4999a01362265393e3ee6ec
carol-wambui <carolwambuidaystar@gmail.com>
2020-10-21 16:58:43 +0300

    change words

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
 function myFunction() {
-    var popup = document.getElementById("myPopup");
-    popup.classList.toggle("show");
-  }
+    event.preventDefault
+    var name = document.getElementById("name").value;
+    var email =document.getElementById("email").value;
+    var Feedback =document.getElementById("Feedback").value;
+
+    alert(name + " Your feedback has been received and will reach out to you through" + email);
+  };
   
